af30a9f67219abd63406840e46653a1801a5de07
novan-p-simanjuntak <novan.p.simanjuntak@gdpventure.com>
2014-01-28 14:34:44 +0700

    Create class HMM model class

---
+++ src/main/java/edu/gdp/nlptools/model/HmmModel.java
@@ -0,0 +1,14 @@
+package edu.gdp.nlptools.model;
+
+public class HmmModel {
+
+	public HmmModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
