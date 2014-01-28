077a8639ad189c6bf8caff373f6d3fcf4e92ae84
novan-p-simanjuntak <novan.p.simanjuntak@gdpventure.com>
2014-01-28 15:02:48 +0700

    Add head annotation in HmmModel

--- src/main/java/edu/gdp/nlptools/model/HmmModel.java
+++ src/main/java/edu/gdp/nlptools/model/HmmModel.java
@@ -1,5 +1,14 @@
 package edu.gdp.nlptools.model;
 
+/**
+ * @file
+ * 
+ *
+ * @author novan.p.simanjuntak <novan.p.simanjuntak@gdpventure.com>
+ * @version 1.1
+ * @since Jan 28, 2014
+ */
+
 public class HmmModel {
 
 	public HmmModel() {
